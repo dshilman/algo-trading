1a9abc44d078274b9eaad4ddfc26f8ec27509699
David Shilman <shilmantech@gmail.com>
2024-01-15 10:39:56 -0500

    STOP_LOSS_ON_FILL_DISTANCE_PRECISION_EXCEEDED

--- trading/trader_strategy_bb_target_sma.py
+++ trading/trader_strategy_bb_target_sma.py
@@ -95,7 +95,12 @@ class BB_to_SMA_Strategy(Strategy):
             if trade_action.spread / trade_action.price >= sl_perc:
                 logger.warning(f"Current spread: {trade_action.spread} is too large for price: {trade_action.price} and sl_perc: {sl_perc}")
                 return None
-            sl_dist = round(trade_action.price * sl_perc, 4)
+            """
+            Have been getting STOP_LOSS_ON_FILL_DISTANCE_PRECISION_EXCEEDED when trading GBP_JPY
+            I assume that the price is too high for 4 digit decimals, thus adding a rule
+            if the price is grater that $100, do not use decimals for stop loss
+            """
+            sl_dist = round(trade_action.price * sl_perc, (4 if trade_action.price < 100 else 0))
 
         else:
             sl_dist = None
