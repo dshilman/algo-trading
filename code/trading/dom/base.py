2e9866a0697dea6578eb7f78525ba6d6eaff52cd
David Shilman <shilmantech@gmail.com>
2024-02-05 08:58:45 -0500

    import issue

--- code/trading/dom/base.py
+++ code/trading/dom/base.py
@@ -17,7 +17,7 @@ class BaseClass ():
             self.logger.setLevel(logging.INFO)
 
 
-    def log(self, message, level = logging.INFO):
+    def log(self, message = None, level = logging.INFO):
         
         if not self.logger == None:
             self.logger.log(level, message)
@@ -25,17 +25,17 @@ class BaseClass ():
             print(message)
 
 
-    def log_warning(self, message):
+    def log_warning(self, message = None):
         self.log(message, logging.WARNING)
 
-    def log_debug(self, message):
+    def log_debug(self, message = None):
         self.log(message, logging.DEBUG)
 
-    def log_error(self, message):
+    def log_error(self, message = None):
         self.log(message, logging.ERROR)
 
-    def log_exception(self, message):
+    def log_exception(self, message = None):
         self.logger.exception(message)
         
-    def log_info(self, message):
+    def log_info(self, message = None):
         self.log(message, logging.INFO)
