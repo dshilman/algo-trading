496fbd878c847cdd990678bc09a375bd4aac9201
David Shilman <shilmantech@gmail.com>
2024-02-05 12:08:14 -0500

    positions defect

--- trader_oanda_all.sh
+++ trader_oanda_all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /home/ec2-user/algo-trading/code/trading
-python3 trading_bot.py AUD_SGD
+echo "execute all"
+./trader_oanda_AUD_SGD.sh & ./trader_oanda_EUR_HKD.sh & ./trader_oanda_EUR_USD.sh & ./trader_oanda_USD_CAD.sh & ./trader_oanda_USD_CHF.sh & ./trader_oanda_USD_HKD.sh &
