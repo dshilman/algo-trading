75c056f0f835e785cc8dd36acadcb68dae4a4650
David Shilman <shilmantech@gmail.com>
2024-02-07 17:01:15 -0500

    inor updates

--- trader_oanda_AUD_SGD.sh
+++ trader_oanda_AUD_SGD.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "Suspending trading bot for AUD_SGD"
 
-# cd /home/ec2-user/algo-trading/code/trading
-# python3 trading_bot.py AUD_SGD
+cd /home/ec2-user/algo-trading/code/trading
+python3 trading_bot.py AUD_SGD
