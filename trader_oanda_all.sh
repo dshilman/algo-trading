12d0ef8e1bddcea3eb2e24767429398f9beaf75d
David Shilman <shilmantech@gmail.com>
2024-02-13 08:11:12 -0500

    check for uptime

--- trader_oanda_all.sh
+++ trader_oanda_all.sh
@@ -3,8 +3,8 @@
 # Check if trading bot scripts are running
 if pgrep trader -l > /dev/null
 then
-    echo "Trading bot is already running."
+    echo $(date) " Trading bot is already running." >>logs/trading/time_check.txt
 else
-    echo "Trading bot is not running. Starting it now..."
+    echo $(date) " Trading bot is not running. Starting it now..."  >>logs/trading/time_check.txt
     ./trader_oanda_AUD_SGD.sh & ./trader_oanda_EUR_HKD.sh & ./trader_oanda_EUR_USD.sh & ./trader_oanda_USD_CAD.sh & ./trader_oanda_USD_CHF.sh & ./trader_oanda_USD_HKD.sh &
 fi
