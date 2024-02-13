48e649dadbe1fd31bdb37561feec9f6a13419abb
David Shilman <shilmantech@gmail.com>
2024-02-13 07:23:01 -0500

    minor optimizations

--- trader_oanda_all.sh
+++ trader_oanda_all.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-echo "execute all"
-./trader_oanda_AUD_SGD.sh & ./trader_oanda_EUR_HKD.sh & ./trader_oanda_EUR_USD.sh & ./trader_oanda_USD_CAD.sh & ./trader_oanda_USD_CHF.sh & ./trader_oanda_USD_HKD.sh &
+#!/bin/bash
+
+# Check if trading bot scripts are running
+if pgrep trader -l > /dev/null
+then
+    echo "Trading bot is already running."
+else
+    echo "Trading bot is not running. Starting it now..."
+    ./trader_oanda_AUD_SGD.sh & ./trader_oanda_EUR_HKD.sh & ./trader_oanda_EUR_USD.sh & ./trader_oanda_USD_CAD.sh & ./trader_oanda_USD_CHF.sh & ./trader_oanda_USD_HKD.sh &
+fi
