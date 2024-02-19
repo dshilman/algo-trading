9ccefa93a1fb154807a0c2a5e1752bd7175741cb
David Shilman <shilmantech@gmail.com>
2024-02-19 10:08:25 -0500

    secret management

--- trader_oanda_all.sh
+++ trader_oanda_all.sh
@@ -3,29 +3,29 @@
 if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh' >/dev/null
 then
     echo $(date) " EUR_HKD Trading bot is already running." 
-    echo $(date) " EUR_HKD Trading bot is already running." >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " EUR_HKD Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
 else
     echo $(date) " EUR_HKD Trading bot is not running. Starting it now..." 
-    echo $(date) " EUR_HKD Trading bot is not running. Starting it now..."  >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " EUR_HKD Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
     ~/algo-trading/trader_oanda_EUR_HKD.sh &
 fi
 
 if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh' >/dev/null
 then
     echo $(date) " USD_HKD Trading bot is already running." 
-    echo $(date) " USD_HKD Trading bot is already running." >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " USD_HKD Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
 else
     echo $(date) " USD_HKD Trading bot is not running. Starting it now..." 
-    echo $(date) " USD_HKD Trading bot is not running. Starting it now..."  >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " USD_HKD Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
     ~/algo-trading/trader_oanda_USD_HKD.sh &
 fi
 
 if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh' >/dev/null
 then
     echo $(date) " EUR_USD Trading bot is already running." 
-    echo $(date) " EUR_USD Trading bot is already running." >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " EUR_USD Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
 else
     echo $(date) " EUR_USD Trading bot is not running. Starting it now..." 
-    echo $(date) " EUR_USD Trading bot is not running. Starting it now..."  >>/home/ec2-user/algo-trading/logs/trading/time_check.txt
+    echo $(date) " EUR_USD Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
     ~/algo-trading/trader_oanda_EUR_USD.sh &
 fi
