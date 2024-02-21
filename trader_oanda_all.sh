c75ed34398f99152e59f6eafae210b59e67139bf
David Shilman <shilmantech@gmail.com>
2024-02-21 17:01:19 -0500

    code refactor

--- trader_oanda_all.sh
+++ trader_oanda_all.sh
@@ -20,12 +20,12 @@ else
     ~/algo-trading/trader_oanda_USD_HKD.sh &
 fi
 
-# if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh' >/dev/null
-# then
-#     echo $(date) " EUR_USD Trading bot is already running." 
-#     echo $(date) " EUR_USD Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
-# else
-#     echo $(date) " EUR_USD Trading bot is not running. Starting it now..." 
-#     echo $(date) " EUR_USD Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
-#     ~/algo-trading/trader_oanda_EUR_USD.sh &
-# fi
+if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh' >/dev/null
+then
+    echo $(date) " EUR_USD Trading bot is already running." 
+    echo $(date) " EUR_USD Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
+else
+    echo $(date) " EUR_USD Trading bot is not running. Starting it now..." 
+    echo $(date) " EUR_USD Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
+    ~/algo-trading/trader_oanda_EUR_USD.sh &
+fi
