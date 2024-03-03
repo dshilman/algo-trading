82ac31d7e14dde5a85e9e7e5f82fa8db2e4e146f
David Shilman <shilmantech@gmail.com>
2024-03-03 15:39:23 -0500

    process management

--- kill_all_oanda.sh
+++ kill_all_oanda.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-
-processes=("/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh"
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh"
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh"
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh"
+processes=("/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh",
+"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh",
+"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh",
+"/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh",
+"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh",
 "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CAD.sh"
 )
 
 # Loop through each process name
 for process_name in "${processes[@]}"; do
+    echo "Stopping $process_name process..."
     # Call kill_process.sh script for each process name
     ./stop_trader_bot.sh "$process_name"
 done
