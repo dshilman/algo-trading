db340b2191a9ce786fde6f3b8c8119ca770cb484
David Shilman <shilmantech@gmail.com>
2024-03-03 15:59:35 -0500

    process management

--- stop_all_trading.sh
+++ stop_all_trading.sh
@@ -2,16 +2,16 @@
 
 processes=("\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh\"",
 "\"python3 trading_bot.py EUR_HKD\"",
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh",
-"python3 trading_bot.py EUR_USD",
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh",
-"python3 trading_bot.py USD_HKD",
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh",
-"python3 trading_bot.py GBP_USD",
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh",
-"python3 trading_bot.py USD_CHF",
-"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CAD.sh",
-"python3 trading_bot.py USD_CAD"
+"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh\"",
+"\"python3 trading_bot.py EUR_USD\"",
+"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh\"",
+"\"python3 trading_bot.py USD_HKD\"",
+"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh\"",
+"\"python3 trading_bot.py GBP_USD\"",
+"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh\"",
+"\"python3 trading_bot.py USD_CHF\"",
+"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CAD.sh\"",
+"\"python3 trading_bot.py USD_CAD\""
 )
 
 # Loop through each process name
