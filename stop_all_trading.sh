05ff2bd86274cdf4a51b28ba491fdd309c03ca13
David Shilman <shilmantech@gmail.com>
2024-03-03 16:35:05 -0500

    process management

--- stop_all_trading.sh
+++ stop_all_trading.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-processes=("\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh\"",
-"\"python3 trading_bot.py EUR_HKD\"",
-"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh\"",
-"\"python3 trading_bot.py EUR_USD\"",
-"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh\"",
-"\"python3 trading_bot.py USD_HKD\"",
-"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh\"",
-"\"python3 trading_bot.py GBP_USD\"",
-"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh\"",
-"\"python3 trading_bot.py USD_CHF\"",
-"\"/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CAD.sh\"",
-"\"python3 trading_bot.py USD_CAD\""
+# Define array of process names
+processes=(
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_HKD.sh"
+    "python3 trading_bot.py EUR_HKD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
+    "python3 trading_bot.py EUR_USD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_HKD.sh"
+    "python3 trading_bot.py USD_HKD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh"
+    "python3 trading_bot.py GBP_USD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh"
+    "python3 trading_bot.py USD_CHF"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CAD.sh"
+    "python3 trading_bot.py USD_CAD"
 )
 
 # Loop through each process name
 for process_name in "${processes[@]}"; do
     echo "Stopping $process_name process..."
-    # ./stop_trader_bot.sh "$process_name"
     pkill -f "$process_name"
 done
