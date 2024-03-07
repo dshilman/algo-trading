e1bb0b56b1b8c3ca68e567fa6e51a36e63156a9e
David Shilman <shilmantech@gmail.com>
2024-03-06 20:09:10 -0500

    refined strategy

--- start_trading.sh
+++ start_trading.sh
@@ -66,6 +66,18 @@ else
     ~/algo-trading/trader_oanda_USD_CHF.sh &
 fi
 
+# EUR_GBP
+if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_GBP.sh' >/dev/null
+then
+    echo $(date) " EUR_GBP Trading bot is already running." 
+    echo $(date) " EUR_GBP Trading bot is already running." >>~/algo-trading/logs/trading/time_check.txt
+else
+    echo $(date) " EUR_GBP Trading bot is not running. Starting it now..." 
+    echo $(date) " EUR_GBP Trading bot is not running. Starting it now..."  >>~/algo-trading/logs/trading/time_check.txt
+    ~/algo-trading/trader_oanda_EUR_GBP.sh &
+fi
+
+
 # EUR_CHF
 if pgrep -f '/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_CHF.sh' >/dev/null
 then
