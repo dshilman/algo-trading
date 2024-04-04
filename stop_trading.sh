2b1bb88c589a02fd0373badf667147a1772c26e6
David Shilman <shilmantech@gmail.com>
2024-04-03 21:24:49 -0400

    strategy update 4-3

--- stop_trading.sh
+++ stop_trading.sh
@@ -2,8 +2,8 @@
 
 # Define array of process names
 processes=(
-    # "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
-    # "python3 trading_bot.py EUR_USD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
+    "python3 trading_bot.py EUR_USD"
     "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh"
     "python3 trading_bot.py USD_CHF"
     "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_GBP.sh"
