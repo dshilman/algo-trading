a6cb9d31ba82e2d1755bdeb22a143e21cf5264e9
David Shilman <shimnatech@gmail.com>
2024-05-15 18:18:58 -0400

    strategy 5/15

--- code/trading/strategies/base/strategy_exec.py
+++ code/trading/strategies/base/strategy_exec.py
@@ -78,8 +78,9 @@ class TradingStrategyExec(TradingStrategyCalc):
         if not self.is_trading_time(trading_time) or self.stop_trading:
             return
 
+         #and (self.std > self.std_mean * self.std_multiplier)#
         if self.long_trading and self.ask < self.bb_low and self.rsi_drop() and 35 >= round(self.rsi, 0) \
-                    and (self.std > self.std_mean * self.std_multiplier) \
+                    and (self.std_mean > self.cv_value) \
                         and self.reverse_rsi_up(trading_time):
                             if not self.backtest:
                                 logger.info(
@@ -87,7 +88,7 @@ class TradingStrategyExec(TradingStrategyCalc):
                             return Trade_Action(self.instrument, self.units_to_trade, self.ask, True, False)
 
         elif self.short_trading and self.bid > self.bb_high and self.rsi_spike() and 65 <= round(self.rsi, 0) \
-                    and (self.std > self.std_mean * self.std_multiplier) \
+                    and (self.std_mean > self.cv_value) \
                         and self.reverse_rsi_down(trading_time):
                             if not self.backtest:
                                 logger.info(
