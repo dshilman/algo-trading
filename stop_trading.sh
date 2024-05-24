f31740f908ff0f0a040d2fadc20c394564188fa3
David Shilman <shimnatech@gmail.com>
2024-05-24 08:31:12 -0400

    strategy 5/24

--- stop_trading.sh
+++ stop_trading.sh
@@ -2,14 +2,14 @@
 
 # Define array of process names
 processes=(
-    # "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
-    # "python3 trading_bot.py EUR_USD"
-    # # "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh"
-    # "python3 trading_bot.py USD_CHF"
-    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_GBP.sh"
-    "python3 trading_bot.py EUR_GBP"
-    # "/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh"
-    # "python3 trading_bot.py GBP_USD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_USD.sh"
+    "python3 trading_bot.py EUR_USD"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_USD_CHF.sh"
+    "python3 trading_bot.py USD_CHF"
+    # "/bin/sh /home/ec2-user/algo-trading/trader_oanda_EUR_GBP.sh"
+    # "python3 trading_bot.py EUR_GBP"
+    "/bin/sh /home/ec2-user/algo-trading/trader_oanda_GBP_USD.sh"
+    "python3 trading_bot.py GBP_USD"
    )
 
 # Loop through each process name
